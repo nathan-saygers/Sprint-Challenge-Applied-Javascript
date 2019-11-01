5445cf803c72004a47168d23c37d488fd641d5a3
nathan-saygers <nathan.saygers@gmail.com>
2019-11-01 08:19:07 -0700

    header complete

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,26 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+  const newHeader = document.createElement('div');
+  const headerDate = document.createElement('span');
+  const headerH1 = document.createElement('h1');
+  const headerTemp = document.createElement('span');
+
+  newHeader.classList.add('header');
+  headerDate.classList.add('date');
+  headerTemp.classList.add('temp');
+
+  newHeader.appendChild(headerDate);
+  newHeader.appendChild(headerH1);
+  newHeader.appendChild(headerTemp);
+
+  headerDate.textContent = 'SMARCH 28, 2019';
+  headerH1.textContent = 'Lambda Times';
+  headerTemp.textContent = '98°';
+
+  return newHeader;
+}
+
+const headerEntry = document.querySelector('.header-container');
+headerEntry.appendChild(Header());
